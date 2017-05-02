8b77a749d6f3d74b78b21cfe85599d9d208153ff
Mikko Kivelä <mikko@smartly.io>
2017-05-02 15:06:42 +0300

    Separate stories into own file

--- src/final.js
+++ src/final.js
@@ -10,42 +10,11 @@ import {
   addLocation
 } from './game/gameActions';
 import gameReducer from './game/gameReducer';
-
-const {stdin, stdout} = process;
-const stories = [
-  {
-    name: 'Circus',
-    solution: [
-      speak('There once was a kingdom. In the middle of the kingdom stood a tall castle. In the castle lived the queen of the land.'),
-      addLocation(1),
-      indicateLocation(1),
-      addCharacter(1, 1),
-      indicateSuccess(1),
-      speak('North of the castle was the village of Norfolk.'),
-      indicateLocation(2),
-      addLocation(2),
-      speak('West of the village lay a road. One day, a travelling circus came along the road.'),
-      indicateLocation(3),
-      addLocation(3),
-      addCharacter(2, 3),
-      speak('The circus arrived into the town to perform their tricks.'),
-      indicateLocation(2),
-      removeCharacter(2, 3),
-      addCharacter(2, 2),
-      speak('The queen heard of the circus and decided to invite them to perform for her.'),
-      indicateLocation(1),
-      removeCharacter(2, 2),
-      addCharacter(2, 1),
-      speak('At the castle, the acrobats wolted, jugglers juggled and magicians casted their spells, but the queen was hard to please. Tired with their tricks the queen told the circus to leave the castle. And so, the circus returned to the road to find new opportunities.'),
-      indicateLocation(3),
-      removeCharacter(2, 1),
-      addCharacter(2, 3),
-    ]
-  }
-];
-let gameState = undefined;
+import stories from './stories';
+import Board from './board';
 
 function updateGameState() {
+  const activeCharacters = board.getState();
 }
 
 function play(currentState) {
@@ -88,6 +57,13 @@ function advance(currentState) {
   return gameReducer(currentState, requiredAction);
 }
 
+const {stdin, stdout} = process;
+let gameState = undefined;
+const board = new Board({
+  characterPins: [2, 3, 4, 5]
+});
+const loop = setInterval(updateGameState, 500);
+
 stdin.resume();
 stdin.setEncoding( 'utf8' );
 stdin.setRawMode(true);
@@ -95,6 +71,8 @@ stdin.on('data', (key, test) => {
   // On CTRL+C (exit)
   if ( key === '\u0003' ) {
     console.log('Exiting');
+    clearInterval(loop);
+    board.stop();
     process.exit();
   }
 
@@ -103,7 +81,7 @@ stdin.on('data', (key, test) => {
     gameState = advance(gameState);
   }
 
-  // Load story
+  // Load story on number press
   const number = parseInt(key) - 1;
   if (number >= 0) {
     if (number < stories.length) {
@@ -115,5 +93,3 @@ stdin.on('data', (key, test) => {
     }
   }
 });
-
-setInterval(updateGameState, 500);
---
+++ src/stories/index.js
@@ -0,0 +1,5 @@
+import circus from './circus';
+
+export default [
+  circus
+];
